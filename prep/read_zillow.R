a93fe66fcca9dcc79850820f06315b05f0e09ce5
Joshh <j-hagedorn@users.noreply.github.com>
2018-12-03 20:12:32 -0500

    Init zillow call

--- prep/read_zillow.R
+++ prep/read_zillow.R
@@ -1,2 +1,7 @@
 library(ZillowR)
-set_zillow_web_service_id('YOUR ZWS-ID')
+set_zillow_web_service_id(Sys.getenv('ZWSID'))
+
+GetDeepSearchResults(
+  address = "1145 Collindale Ave NW",
+  citystatezip = "Grand Rapids, MI 49504"
+)
